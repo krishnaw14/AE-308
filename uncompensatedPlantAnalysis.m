b5dda2400ae8a21cce623dd1cc12d5691e1b37b5
krishnaw14 <krishnaw14@gmail.com>
2018-11-04 05:04:36 +0530

    Uncompensated Plant Characterestics

---
+++ uncompensatedPlantAnalysis.m
@@ -0,0 +1,30 @@
+% To analyse the uncompensated plant characterestics to be later compared
+% with the compensated plant. 
+
+s = tf('s');
+G = 1/((s+1)*(s+2)*(s+4)*(s+6));
+
+figure(1)
+rlocus(G);
+
+figure(2)
+margin(G);
+
+figure(3)
+bode(G);
+
+figure(4)
+nyquist(G);
+
+figure(5)
+step(feedback(G,1));
+
+disp(stepinfo(feedback(G,1)));
+fprintf('\n');
+disp(bandwidth(G));
+fprintf('\n');
+fprintf('Closed Loop poles: ');
+disp(pole(feedback(G,1)));
+fprintf('\n');
+
+
